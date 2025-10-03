5f546ebaeb92a614762ced2ac0d48591eb50aad5
CabbaGG2 <jcamaradepablos@danielcastelao.org>
2025-10-03 14:37:59 +0200

    Se implemento el códido de la parte 3.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException, IllegalArgumentException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, IllegalArgumentException, XMLStreamException {
         Producto p1 = new Producto("Manzana", 100, 0.5);
         SerializarService serializarService = new SerializarService();
         serializarService.SerializarProducto(p1);
@@ -15,5 +16,8 @@ public class Main {
 
         ProductoTransient p4 = (ProductoTransient) serializarService.DeserializarProductoT("serialT");
         System.out.println(p4);
+
+        XMLService xmlService = new XMLService();
+        xmlService.crearXML("1", "a1", "Alexandre Dumas", "El Conde de Montecristo", "Los miserables");
     }
 }
---
+++ src/XMLService.java
@@ -0,0 +1,43 @@
+import javax.xml.parsers.SAXParser;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class XMLService {
+    private static final String XML_FILE = "autores.xml";
+
+    public SAXParser crearXML(String autor, String codigo, String nombre, String titulo, String titulo2) throws IOException, XMLStreamException {
+        XMLOutputFactory factory = XMLOutputFactory.newInstance();
+        try {
+             XMLStreamWriter writer = factory.createXMLStreamWriter((new FileWriter(XML_FILE)));
+
+            writer.writeStartDocument("1.0");
+            writer.writeStartElement("autores");
+            writer.writeStartElement("autor");
+            writer.writeAttribute("codigo", codigo);
+            writer.writeStartElement("nombre");
+            writer.writeCharacters(nombre);
+            writer.writeEndElement();
+            writer.writeStartElement("titulo");
+            writer.writeCharacters(titulo);
+            writer.writeEndElement();
+            writer.writeStartElement("titulo");
+            writer.writeCharacters(titulo2);
+            writer.writeEndElement();
+            writer.writeEndElement();
+            writer.writeEndDocument();
+            writer.flush();
+            writer.close();
+            System.out.println("Archivo 'autores.xml' creado con exito.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+}
